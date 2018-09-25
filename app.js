b036cb1c4ae70871256ec2e75393efe2f739834e
Nikita <lecannik@gmail.com>
2018-09-25 17:16:25 +0600

    test

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ console.log("Server start");
 
 //create a server object:
 http.createServer(function (req, res) {
+    console.log(res);
     res.write('Hello World!'); //write a response to the client
     res.end(); //end the response
 }).listen(3000); //the server object listens on port 8080
